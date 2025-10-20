a46939320eb87c1779b21cb1d948aee729b86884
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2025-10-20 17:09:49 +0600

    a few design updated in css file and initial the variable in js file.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const namesInput = document.querySelector('#nameInput');
+const drawBtn = document.querySelector('#drawBtn');
+const winnerModal = document.querySelector('#winnerModal');
+const winnerName = document.querySelector('#winnerName');
+const closeModal = document.querySelector('#closeModal');
+const namesPreview = document.querySelector('#namesPreview');
+const spinner = document.querySelector('#spinner');
+const confettiCanvas = document.querySelector('#confettiCanvas');
+
+const ctx = confettiCanvas.getContext('2d');
